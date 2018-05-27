3c7bd977141b033b741e5b25d8ea8a636613f59a
Dario Vladovic <d.vladimyr@gmail.com>
2018-05-27 17:23:07 +0200

    Implement cowsay-like duckiesays.com client
    - supporting arguments based input and piping

---
+++ README.md
@@ -0,0 +1,6 @@
+# {{name}} [![npm package version](https://img.shields.io/npm/v/{{name}}.svg)](https://npm.im/{{name}}) [![github license](https://img.shields.io/github/license/vladimyr/{{name}}.svg)](https://github.com/vladimyr/{{name}}/blob/master/LICENSE) [![js-semistandard-style](https://img.shields.io/badge/code%20style-semistandard-brightgreen.svg)](https://github.com/Flet/semistandard)
+
+>{{description}}
+
+This is base template for all my npm packages. :tada:
+
---
+++ cli.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const cowsay = require('cowsay-tag');
+const getStdin = require('get-stdin');
+const got = require('got');
+const meow = require('meow');
+const pkg = require('./package.json');
+const urlJoin = require('url-join');
+
+const trimLines = str => str.replace(/^\n+/, '').replace(/\n+$/, '');
+
+const duck = trimLines(`
+  \\
+   \\ >()_
+      (__)__ _
+`);
+
+const help = `
+${duckiesay('What does the duckie say?\nQuack, quack.')}
+
+Usage
+  $ ${pkg.name} <input>
+
+Options
+  -h, --help     Show help
+  -v, --version  Show version number
+
+Examples
+  $ duckiesay  # gets random quote from remote api
+  $ ${pkg.name} quack quack
+  $ echo 'quack quack' | duckiesay
+
+Homepage:     ${pkg.homepage}
+Report issue: ${pkg.bugs.url}`;
+
+const flags = {
+  help: { alias: 'h' },
+  version: { alias: 'v' }
+};
+
+(async () => {
+  const cli = meow(help, { flags });
+  const input = cli.input.join(' ') || await getStdin();
+  const [message, id] = input ? [input] : await getQuote();
+  console.log(duckiesay(message));
+  if (!id) return;
+  console.log();
+  console.log(urlJoin(pkg.config.url, `/${id}`));
+})();
+
+function duckiesay(message, cow = duck) {
+  return cowsay(cow)`${message.trim()}`;
+}
+
+async function getQuote(quote = '') {
+  const url = urlJoin(pkg.config.apiUrl, `/says/${quote}`);
+  const { id, says } = (await got(url, { json: true })).body;
+  return [says.replace(/(\.\s*)(?!$)/g, '.\n'), id];
+}
