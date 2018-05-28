36e3b07a98cf4f8ed35f0ca2d551472b990ba4e0
Toma Zelic <zelic.toma@gmail.com>
2018-05-28 14:29:36 +0200

    Animate little duck and make it more picturesque :rocket:

--- cli.js
+++ cli.js
@@ -2,10 +2,11 @@
 
 'use strict';
 
+const boxen = require('boxen');
 const chalk = require('chalk');
-const cowsay = require('cowsay-tag');
 const getStdin = require('get-stdin');
 const got = require('got');
+const logUpdate = require('log-update');
 const meow = require('meow');
 const pkg = require('./package.json');
 const urlJoin = require('url-join');
@@ -13,31 +14,46 @@ const urlJoin = require('url-join');
 const trimLines = str => str.replace(/^\n+/, '').replace(/\n+$/, '');
 const splitSentences = str => str.replace(/(\.\s*)(?!$)/g, '.\n');
 
-const bwDuck = trimLines(`
-  \\
-   \\ >()_
-    __(__)__ _
+const beaks = ['>', '='];
+
+let i = 0;
+
+const duck = frame => trimLines(chalk`
+   \\
+    \\  {yellow _
+      {red ${frame}}(.)__
+       (___/}
+{blue    ~ ~~~~~~~~~~~~~ ~
+  ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+   ~ ~ ~ ~ ~ ~ ~ ~ ~
+      ~ ~ ~ ~ ~ ~}
 `);
 
-const duck = trimLines(chalk`
-  \\
-   \\ {red >}{yellow ()_}
-    {blue __}{yellow (}{blue __}{yellow )}{blue __ _}
+const bwDuck = frame => trimLines(`
+   \\
+    \\   _
+      ${frame}(.)__
+       (___/
+  ~ ~~~~~~~~~~~~~ ~
+ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
+  ~ ~ ~ ~ ~ ~ ~ ~ ~
+     ~ ~ ~ ~ ~ ~
 `);
 
-const banner = [
-  ['What does the duckie say?', chalk`{red What does the {bold.yellow duckie} say?}`],
-  ['Quack, quack.', chalk`{yellow Quack, quack.}`]
-];
+const banner = chalk`
+{red What does the {bold.yellow duckie} say?}
+{yellow Quack, quack.}`;
 
 const help = chalk`
-${outputBanner(banner)}
+${messageBox(banner)}
+${duck(beaks[0])}
 
 Usage
   $ ${pkg.name} <input>
 
 Options
   --no-color     Use black'n'white duck
+  --no-animation Use still duck
   -h, --help     Show help
   -v, --version  Show version number
 
@@ -52,36 +68,40 @@ Report issue: {green ${pkg.bugs.url}}`;
 const flags = {
   help: { alias: 'h' },
   version: { alias: 'v' },
-  color: { type: 'boolean', default: true }
+  color: { type: 'boolean', default: true },
+  animation: { type: 'boolean', default: true }
 };
 
 (async () => {
   const cli = meow(help, { flags });
   const input = cli.input.join(' ') || await getStdin();
   const [message, id] = input ? [input] : await getQuote();
-  console.log(duckiesay(message, cli.flags.color));
-  if (!id) return;
-  console.log();
-  console.log(urlJoin(pkg.config.url, `/${id}`));
+  if (id) {
+    console.log(urlJoin(pkg.config.url, `/${id}`));
+    console.log();
+  }
+  duckiesay(message, cli.flags.color, cli.flags.animation);
 })();
 
-function duckiesay(message, color = true) {
-  const cow = color ? duck : bwDuck;
-  return cowsay(cow)`${message.trim()}`;
+function duckiesay(message, color = true, animation = true) {
+  const duckie = color ? duck : bwDuck;
+  console.log(messageBox(message));
+  if (!animation) return console.log(duckie(beaks[0]));
+
+  const interval = setInterval(() => {
+    const beak = beaks[i = ++i % beaks.length];
+    logUpdate(duckie(beak));
+  }, 200);
+
+  setTimeout(() => clearInterval(interval), 3000);
+}
+
+function messageBox(message) {
+  return boxen(message.trim(), { padding: 1, borderStyle: 'round' });
 }
 
 async function getQuote(quote = '') {
   const url = urlJoin(pkg.config.apiUrl, `/says/${quote}`);
   const { id, says } = (await got(url, { json: true })).body;
   return [splitSentences(says), id];
-}
-
-// NOTE: This is necessary because `cowsay-tag`
-//       does NOT support ansi escape sequences!
-function outputBanner(lines) {
-  const out = duckiesay(lines.map(it => it[0]).join('\n'));
-  return lines.reduce((acc, [str, colorized]) => {
-    acc = acc.replace(str, colorized);
-    return acc;
-  }, out);
 }
